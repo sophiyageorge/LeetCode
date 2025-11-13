4042972528de406f68db54dc4b59f27e963ac7ae
Mary Sophiya <47908915+sophiyageorge@users.noreply.github.com>
2025-11-13 10:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (35.21%) - LeetSync

--- 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.py
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.py
@@ -1,24 +1,15 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         
-        # n = len(nums)
-        # is_added = [False]*k
-        # count =0
-        # for i in range(n-1,-1,-1):
-        #     if nums[i]>k or is_added[nums[i]-1]:
-        #         continue
-        #     is_added[nums[i]-1] = True
-        #     count += 1
-        #     if count == k :
-        #         return n-i
-
+       
         seen = set()
 
         for i,num in enumerate(reversed(nums)):
-            if num>k:
+            if num>k :
                 continue
             seen.add(num)
-            if k==len(seen):
+
+            if len(seen)==k:
                 return i+1
 
    
